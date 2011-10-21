288d5a7cfcd8d0e57edaf387dbe486856f60afc8
Yuanhao Sun <flakever@gmail.com>
2011-10-21 17:38:56 +0200

    Online classifier works now

--- apps/classifier/views.py
+++ apps/classifier/views.py
@@ -1,28 +1,31 @@
 from django.shortcuts import get_object_or_404, redirect, render_to_response
 from django.template import RequestContext
 
-from nltk.book import *
+# from nltk.book import *
 
 # Test using other module
 from hello import Hellow
 hi = Hellow()
 
-from classifier import classify
+from classifier import classification
 
-texts = [text1, text2, text3, text4, text5, text6, text7, text8, hi]
+# texts = [text1, text2, text3, text4, text5, text6, text7, text8, hi]
+texts = ['SVM', 'NB', 'SGC', 'kNN']
+
 
 def home(request):
     return render_to_response('home.html', RequestContext(request, dict(texts=texts)))
 
 def classify(request):
     if request.method == "POST":
-        corpus = texts[int(request.POST['corpus'])]
+        algo = texts[int(request.POST['class_algo'])]
         word = request.POST['word']
-        count = corpus.count(word)
+        # count = corpus.count(word)
+        category = classification(word)
 
         context = {
-            'count' : count,
+            'category' : category,
             'word' : word,
-            'corpus' : corpus,
+            'algo' : algo,
             }
         return render_to_response('results.html', RequestContext(request, context))
